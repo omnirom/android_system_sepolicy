c46c0124dc6cce34f89b20ee07d8dbc2b5401625
Humberto Borba <humberos@gmail.com>
2016-11-01 09:17:10 -0200

    system: sepolicy: Fix the logic for camera permissions
    https://gerrit.omnirom.org/#/c/19484/
    This have introduced a new one domain called "camera" which
    will be used for specific device to prevent neverallow errors.
    
    This rule is generic then all devices will be affected by it.
    
    Also, this new domain (camera) requires its type declaration
    which was removed in https://gerrit.omnirom.org/#/c/19977/
    
    Now all devices should include this declaration on its sepolicy
    specific rules.
    
    So, just avoid the massive changes in devices trees then the
    logic of this rule should done in device makefile (omni_[device-name]).
    
    To use the specific camera permission, this flag is required:
    
        TARGET_USES_CUSTOM_CAMERA_PERMISSION := true
    
    Also, make sure that camera domain is present in camera.te file.
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: Ib1fa3293c8830c44d83609f32f10c0ccf500b79c

--- Android.mk
+++ Android.mk
@@ -96,6 +96,7 @@ $(sepolicy_policy.conf): $(call build_policy, $(sepolicy_build_files))
 		-D mls_num_sens=$(PRIVATE_MLS_SENS) -D mls_num_cats=$(PRIVATE_MLS_CATS) \
 		-D target_build_variant=$(TARGET_BUILD_VARIANT) \
 		-D omni_device=$(TARGET_PRODUCT) \
+		-D custom_camera_perm=$(TARGET_USES_CUSTOM_CAMERA_PERMISSION) \
 		-s $^ > $@
 	$(hide) sed '/dontaudit/d' $@ > $@.dontaudit
 
